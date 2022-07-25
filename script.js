4613b26b150629875189f3ec3e674f161df8b987
Melina Alegre <Melina Alegre>
2022-07-25 20:52:19 -0300

    agregue comentarios

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let n
 let repetir = true
 do {
-    n = parseInt(prompt("Ingrese la cantidad de productos que desea comprar")) // Pide un número
+    n = parseInt(prompt("Ingrese la cantidad de productos que desea comprar")) // Pide un número, cantidad de productos.
     if (isNaN(n)) {
-        alert("Por favor, ingrese un número válido") 
+        alert("Por favor, ingrese un número válido") // Si el usuario ingresa otra cosa que no sea un número, envio una alerta.
     } else {
         repetir = false
     }
 } while(repetir == true)
 
-for (let i = 0; i < n; i = i + 1) { // Me aseguro de que el ciclo se repita n veces
+for (let i = 0; i < n; i = i + 1) { // Me aseguro de que el ciclo se repita n veces.
     alert(`El producto ${i+1} se ingresó al carrito`)
 }
 

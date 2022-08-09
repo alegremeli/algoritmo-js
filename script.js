b0e898b8714c6767a3624af075c7d8dea5970410
Melina Alegre <Melina Alegre>
2022-08-08 23:44:55 -0300

    agregado de eventos

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let carrito = [] //Creo un array vacío, que se ira completando con los producto
 let producto1 = {
     nombre: "Budín",
     precio: 400,
-    tipo: "sinTacc"
+    tipo: "Sin Tacc"
 }
 
 let producto2 = {
@@ -21,53 +21,45 @@ let producto3 = {
 let producto4 = {
     nombre: "Donas",
     precio: 500,
-    tipo: "sinTacc"
+    tipo: "Sin Tacc"
 }
 
 let productos = [producto1, producto2, producto3, producto4]
 
-let ofertas = productos.filter(producto => producto.precio < 500) //Filtra entre los productos, para saber el mejor precio, que luego sera una oferta.
-console.log(ofertas)
 
-let productosSinTacc = productos.filter((el) => el.tipo.includes('sinTacc')) //Filtra entre los productos dependiendo su tipo.
-console.log(productosSinTacc)
+//Creo las cards de todos los productos que tenga, de manera general.
+productos.forEach((producto, i) => {
+    let divProducto = document.createElement("div");
+    divProducto.innerHTML = `
+    <div>
+        <div id="cartaP${i+1}" class="card bordeCartas" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${producto.nombre}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">$${producto.precio}</h6>
+            <p class="card-text">${producto.tipo}</p>
+        </div>
+        <button id="botonCarta${i+1}" type="button" class="btn btn-primary">Agregar</button>
+    </div>
+    `
 
-let nombreProductos = ""
+    let divCartas = document.getElementById("cartas")
+    divCartas.append(divProducto)
 
-for (let elemento of productos) {
-    nombreProductos = nombreProductos + elemento.nombre + "/"
-}
+})
 
-function elegir() { // Sólo funciona si los productos ingresados están bien escritos
-    let eleccion = prompt(`Que producto desea agregar?\n${nombreProductos.slice(0, -1)} \n Budínes y Donas sin Tacc. Muffins veganos. :)`)
+//Creo una clase para el botón de cada uno de mis productos, así cuando el usuario hace "click",
+//puede agregarlo a su carrito.
 
-    for (let elemento of productos) {
-        if (elemento.nombre == eleccion) {
-            carrito.push(elemento)
-        }
-    }
-}
-
-let repetir
-
-do { // Sólo funciona si en el prompt se responde "si" o "no" 
-    elegir()
-
-    let respuesta = prompt("¿Desea agregar otro producto? (si/no)")
-
-    if (respuesta == "si") { //Vuelve al principio.
-        repetir = true
-    } else if (respuesta == "no") { //Retorna el nombre del producto y su valor.
-        repetir = false
-    }
-} while (repetir)
-
-console.log(carrito)
-
-let i = 1 // Reemplacé los tres for tradicionales por for...of
-for (let producto of carrito) { //Retorna el nombre del producto y su valor.
-    let compra = document.createElement("div");
-    compra.innerHTML = `<h2>El producto elegido N°${i} es ${producto.nombre}, y vale $${producto.precio}</h2>`;
-    document.body.appendChild(compra);
-    i = i + 1
-}
+productos.forEach((producto, i) => {
+    let botonCartas = document.getElementById(`botonCarta${i+1}`)
+    botonCartas.addEventListener("click", () => {
+        let divCarrito = document.getElementById("carrito")
+        divCarrito.innerHTML += `
+    <div class="card-body bordeCartas">
+        <h5 class="card-title">${producto.nombre}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">$${producto.precio}</h6>
+        <p class="card-text">${producto.tipo}</p>
+    </div>
+    `
+    })
+})

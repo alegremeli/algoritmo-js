95975db5afcce5324821ae3e2c6aa0673c7bef72
Melina Alegre <Melina Alegre>
2022-08-15 20:30:24 -0300

    agregue local storage, segunda preentrega

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let carrito = [] //Creo un array vacío, que se ira completando con los productos.
+let carrito = {elementosCarrito : ""} //Creo un array vacío, que se ira completando con los productos.
+
+if (localStorage.getItem("claveCarrito")) {
+    carrito = JSON.parse(localStorage.getItem("claveCarrito")) // Si existe un objeto con clave "claveCarrito", lo obtiene
+    document.getElementById("carrito").innerHTML = carrito.elementosCarrito // Agregamos al div con id "carrito" el carrito en formato HTML guardado en localstorage
+} else {
+    localStorage.setItem("claveCarrito", JSON.stringify(carrito)) // Si no existe, lo crea
+}
 
 let producto1 = {
     nombre: "Budín",
@@ -24,11 +31,12 @@ let producto4 = {
     tipo: "Sin Tacc"
 }
 
-let productos = [producto1, producto2, producto3, producto4]
+let productos = [producto1, producto2, producto3, producto4]    
 
 
 //Creo las cards de todos los productos que tenga, de manera general.
 productos.forEach((producto, i) => {
+
     let divProducto = document.createElement("div");
     divProducto.innerHTML = `
     <div>
@@ -50,16 +58,29 @@ productos.forEach((producto, i) => {
 //Creo una clase para el botón de cada uno de mis productos, así cuando el usuario hace "click",
 //puede agregarlo a su carrito.
 
+let botonCartas
+
 productos.forEach((producto, i) => {
-    let botonCartas = document.getElementById(`botonCarta${i+1}`)
+    botonCartas = document.getElementById(`botonCarta${i+1}`)
     botonCartas.addEventListener("click", () => {
         let divCarrito = document.getElementById("carrito")
         divCarrito.innerHTML += `
-    <div class="card-body bordeCartas">
-        <h5 class="card-title">${producto.nombre}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">$${producto.precio}</h6>
-        <p class="card-text">${producto.tipo}</p>
-    </div>
-    `
+        <div class="card-body bordeCartas">
+            <h5 class="card-title">${producto.nombre}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">$${producto.precio}</h6>
+            <p class="card-text">${producto.tipo}</p>
+        </div>
+        `
+        // Esto actualiza el objeto con clave "claveCarrito" guardado en el localstorage
+        carrito.elementosCarrito += `
+        <div class="card-body bordeCartas">
+            <h5 class="card-title">${producto.nombre}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">$${producto.precio}</h6>
+            <p class="card-text">${producto.tipo}</p>
+        </div>
+        `
+
+        localStorage.setItem("claveCarrito", JSON.stringify(carrito))
+
     })
 })
